6284fe6478985ff94f138ff5e3b748a7ed463c2f
ericmachmer@gmail.com <ericmachmer@gmail.com>
2025-04-10 22:42:40 -0700

    implement main function for micro:bit with RTT logging

--- hello_microbit/src/main.rs
+++ hello_microbit/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+// `entry` is the entry point of the program, `main` is not assumed because of `no_main`
+use cortex_m_rt::entry;
+
+// the `microbit` crate provides the API for the micro:bit
+// if this is omitted, the compile will fail
+use microbit as _;
+
+// `rtt_target` is used for RTT logging
+use rtt_target::{rtt_init_print,rprintln};
+
+// `panic_rtt_target` is used for panic messages
+use panic_rtt_target as _;
+
+#[entry]
+fn main() -> ! {
+    // initialize RTT logging
+    rtt_init_print!();
+
+    loop {
+        // print a message to the RTT log
+        rprintln!("Hello from micro:bit!");
+    }
 }
